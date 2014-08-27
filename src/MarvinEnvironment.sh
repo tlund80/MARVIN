a4d1706fa325c52fcdf56764487d4ec726fb558b
tsolund <tsolund@tek-marvin-1.sandbox.tek.sdu.dk>
2014-08-27 10:36:45 +0200

    changed MarvinEnvironment.sh to fit the MARVIN platform

--- src/MarvinEnvironment.sh
+++ src/MarvinEnvironment.sh
@@ -14,16 +14,18 @@
 
 #*** Marvin Configuration ***
 # DTI Paths
-export DTI_CO_WORKER_ROOT=~/dti_co_worker/trunk/MARVIN
-export DTI_COMMON_ROOT=~/dti_co_worker/trunk/common
+export DTI_CO_WORKER_ROOT=~/MARVIN
+#export DTI_COMMON_ROOT=~/dti_co_worker/trunk/common
 
 #*** ROS Configuration ***
 # ROS Paths
-HYDRO_SETUP=/opt/ros/hydro/setup.bash
+#HYDRO_SETUP=/opt/ros/hydro/setup.bash
+INDIGO_SETUP=/opt/ros/indigo/setup.bash
 COWORKER_SETUP=$DTI_CO_WORKER_ROOT/devel/setup.sh
 
-if [ -f $HYDRO_SETUP ]; then
-  source $HYDRO_SETUP
+
+if [ -f $INDIGO_SETUP ]; then
+  source $INDIGO_SETUP
 else
   echo "Warning: Unable to source '$HYDRO_SETUP'"
 fi
@@ -39,13 +41,14 @@ fi
 #ROS URI to find the Master server
 export ROS_MASTER_URI=http://$MASTER_IP:11311
 
-export RobWork_DIR=~/dti_co_worker/trunk/common/RobWork/RW_new/RobWork
+export RobWork_DIR=~/RobWork
 export RW_ROOT=$RobWork_DIR/RobWork/
 export RWS_ROOT=$RobWork_DIR/RobWorkStudio/
 export RWHW_ROOT=$RobWork_DIR/RobWorkHardware/
 export RWSIM_ROOT=$RobWork_DIR/RobWorkSim/
+export RobWorkSim_DIR=$RWSIM_ROOT
 
-export CoViS_DIR=$DTI_COMMON_ROOT/CoViS/build/lib/cmake/CoViS
+export CoViS_DIR=~/CoViS/build/lib/cmake/CoViS
 
 #export RobWork_DIR=~/dti_co_worker/trunk/common/RobWork/RW_new/RobWork
 #export RobWorkHardware_DIR=$RobWork_DIR/RobWorkHardware
